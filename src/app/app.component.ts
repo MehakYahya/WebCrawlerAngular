32326bf86863908ed7d3450cf113301fa5af8524
Mehak Yahya <168831879+MehakYahya@users.noreply.github.com>
2024-11-29 01:30:04 +0500

    Create app.component.ts

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { BbcCrawlerComponent } from './bbc-crawler/bbc-crawler.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,  // Ensures the component can work on its own
+  imports: [BbcCrawlerComponent],
+  templateUrl: './app.component.html',
+
+})
+export class AppComponent {}
