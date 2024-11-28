b042c5bcaf3e2f6bf8a049c9e3f0861654efaf93
Mehak Yahya <168831879+MehakYahya@users.noreply.github.com>
2024-11-29 01:29:46 +0500

    Create app.config.ts

---
+++ src/app/app.config.ts
@@ -0,0 +1,10 @@
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+export const appConfig = {
+  providers: [
+    importProvidersFrom(HttpClientModule),
+  ],
+};
+
+export class AppSettings {}
